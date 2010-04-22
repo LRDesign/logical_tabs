ec96bdd3464e6ad5d6e6334cc956839dca4a7f6a
Evan <evan@lrdesign.com>
2010-04-21 18:22:50 -0700

    added adding of tabs, including protection against duplicates.

--- lib/logical_tabs/tabbed_panel.rb
+++ lib/logical_tabs/tabbed_panel.rb
@@ -1,7 +1,11 @@
 module LogicalTabs
+  class DuplicateTabID < Exception
+  end
+  
   # a collection of tabs with their associated panes, I.E. a tabbed panel
   class TabbedPanel  
-
+    attr_reader :tabs
+    
     # view must be an instance of ActionView::Base.   This class 
     # depends on it for access to the capture and content_tag methods
     def initialize(view)
@@ -9,10 +13,31 @@ module LogicalTabs
       @tabs = []
     end
 
+    # Add a new tab (with pane) to this tabbed panel.   Either pass
+    # a block with the content for the panel, or pass :content => 'my_content'.
+    #
+    # Name is required and serves as the internal identifier for the tab.  By
+    # default, it is also the displayed text of the tab.
+    #
+    #
+    # Other options:
+    #   :base_id  The string used as the beginning of the ID for CSS
+    #             IDs for the tab and pane.  :base_id => 'foo' will
+    #             generate a tab with ID 'foo_tab' and a panel with ID
+    #             'foo_panel'.  Defaults to name.downcase
+    #
+    #   :tab_text The text to display in the tab.  Defaults to name.
+    #
+    #   :content  The content for the panel itself, in HTML
     def add_tab(name, options = {},  &block)       
       # debugger  
       options[:content] ||= v.capture(&block)
-      @tabs << TabPane.new(self, name, options)
+      tab = TabPane.new(self, name, options)
+      if @tabs.any?{ |t| t.base_id == tab.base_id } 
+        raise DuplicateTabID
+      else
+        @tabs << tab
+      end
     end
 
     def render_tabs
@@ -33,6 +58,11 @@ module LogicalTabs
 
     def selected?(tab)
       tab == selected_tab
+    end
+
+    # returns the array of TabPanes
+    def tabs
+      @tabs
     end
 
     # shortcut to the View
--- spec/views/tabbed_panel_spec.rb
+++ spec/views/tabbed_panel_spec.rb
@@ -9,4 +9,30 @@ describe LogicalTabs::TabbedPanel do
   it "should initialize successfully" do
     LogicalTabs::TabbedPanel.new(@view)  
   end
+  
+  describe "adding tab_panes" do
+    describe "with specified content" do
+      before(:each) do
+        @panel = LogicalTabs::TabbedPanel.new(@view)
+      end
+      it "should create a panel with one tab" do
+        @panel.add_tab('foo', :content => 'Lorem Ipsum')
+        @panel.tabs.should have(1).tab
+      end      
+      it "should create a panel with one tab with the right content" do
+        @panel.add_tab('foo', :content => 'Lorem Ipsum')
+        @panel.tabs[0].content.should == 'Lorem Ipsum'       
+      end
+      it "should create a tab with a specific id" do
+        @panel.add_tab('foo', :content => 'Lorem Ipsum', :base_id => 'bar')
+        @panel.tabs[0].base_id.should == 'bar'               
+      end
+      it "should raise an exception if a tab with duplicate ID is added" do
+        lambda do 
+          @panel.add_tab('foo', :content => 'Lorem Ipsum', :base_id => 'bar')
+          @panel.add_tab('foo 2', :content => 'Lorem Ipsum 2', :base_id => 'bar')
+        end.should raise_error 
+      end
+    end  
+  end
 end
