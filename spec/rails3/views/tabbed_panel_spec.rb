77d2f9b506bc3e127557ae928b72eacc4b07c975
Evan Dorn <evan@lrdesign.com>
2011-02-24 11:01:14 -0800

    started converting old class specs to webrat format

--- spec/rails3/views/tabbed_panel_spec.rb
+++ spec/rails3/views/tabbed_panel_spec.rb
@@ -7,9 +7,9 @@ describe LogicalTabs::TabbedPanel do
     @view = ActionView::Base.new
   end
   it "should initialize successfully" do
-    LogicalTabs::TabbedPanel.new(@view)  
+    LogicalTabs::TabbedPanel.new(@view)
   end
-  
+
   describe "generating tabbed panels" do
     it "should generate a tabbed panel" do
       @panel = LogicalTabs::TabbedPanel.new(@view)
@@ -17,74 +17,74 @@ describe LogicalTabs::TabbedPanel do
     end
     it "should append an id to the tabbed panel" do
       @panel = LogicalTabs::TabbedPanel.new(@view, :seq => 0)
-      @panel.base_id.should == "tabbed_panel_0"  
+      @panel.base_id.should == "tabbed_panel_0"
     end
     it "should use a specified base_id rather than a sequence if specified" do
-      @panel = LogicalTabs::TabbedPanel.new(@view, :base_id => 'my_panel')            
-      @panel.base_id.should == "my_panel"         
+      @panel = LogicalTabs::TabbedPanel.new(@view, :base_id => 'my_panel')
+      @panel.base_id.should == "my_panel"
     end
   end
-  
+
   describe "rendering the output" do
     before(:each) do
       @panel = LogicalTabs::TabbedPanel.new(@view)
-      @panel.add_tab('foo', :content => "Lorem ipsum")  
-      @panel.add_tab('bar', :content => "dolor sit amet.")  
+      @panel.add_tab('foo', :content => "Lorem ipsum")
+      @panel.add_tab('bar', :content => "dolor sit amet.")
     end
-    
+
     describe "rendering the whole panel" do
       before(:each) { @output = @panel.render }
       it "should render an outer div" do
-        @output.should have_tag('div#tabbed_panel.tabbed_panel')
+        @output.should have_selector('div#tabbed_panel.tabbed_panel')
       end
-      it "should render an outer div containing the tabs and panes" do        
-        @output.should have_tag('div#tabbed_panel') do
-          with_tag('ul.tabs')
-          with_tag('div.panes')
-        end        
+      it "should render an outer div containing the tabs and panes" do
+        @output.should have_selector('div#tabbed_panel') do |scope|
+          scope.should have_selector('ul.tabs')
+          scope.should have_selector('div.panes')
+        end
       end
     end
-    
+
     describe "rendering tabs" do
       before(:each) do
         @output = @panel.render_tabs
       end
       it "should render a url" do
-        @output.should have_tag("ul.tabs")
+        @output.should have_selector("ul.tabs")
       end
       it "should render an li for each tab" do
-        @output.should have_tag("ul") do
-          with_tag ("li##{@panel.base_id}_tp_foo_tab")
-          with_tag ("li##{@panel.base_id}_tp_bar_tab")
-        end        
+        @output.should have_selector("ul") do |scope|
+          scope.should have_selector("li##{@panel.base_id}_tp_foo_tab")
+          scope.should have_selector("li##{@panel.base_id}_tp_bar_tab")
+        end
       end
-      it "should render the first li selected by default" do
-        @output.should have_tag("li##{@panel.base_id}_tp_foo_tab.tab_selected")
-        @output.should have_tag("li##{@panel.base_id}_tp_bar_tab.tab_unselected")        
+      it "should render the first li selected by default" do |scope|
+        @output.should have_selector("li##{@panel.base_id}_tp_foo_tab.tab_selected")
+        @output.should have_selector("li##{@panel.base_id}_tp_bar_tab.tab_unselected")
       end
     end
-    
+
     describe "rendering the panes" do
       before(:each) do
         @output = @panel.render_panes
       end
       it "should render an outer div of class panes" do
-        @output.should have_tag('div.panes')
+        @output.should have_selector('div.panes')
       end
       it "should render a div for each tab" do
-        @output.should have_tag('div.panes') do         
-          with_tag("div##{@panel.base_id}_tp_foo_pane")
-          with_tag("div##{@panel.base_id}_tp_bar_pane")
+        @output.should have_selector('div.panes') do |scope|
+          scope.should have_selector("div##{@panel.base_id}_tp_foo_pane")
+          scope.should have_selector("div##{@panel.base_id}_tp_bar_pane")
         end
       end
       it "should render the first div selected by default" do
-        @output.should have_tag("div##{@panel.base_id}_tp_foo_pane.pane_selected")
-        @output.should have_tag("div##{@panel.base_id}_tp_bar_pane.pane_unselected")
-      end      
+        @output.should have_selector("div##{@panel.base_id}_tp_foo_pane.pane_selected")
+        @output.should have_selector("div##{@panel.base_id}_tp_bar_pane.pane_unselected")
+      end
     end
   end
-  
-  
+
+
   describe "adding tab_panes" do
     before(:each) do
       @panel = LogicalTabs::TabbedPanel.new(@view)
@@ -93,31 +93,31 @@ describe LogicalTabs::TabbedPanel do
       it "should create a panel with one tab" do
         @panel.add_tab('foo', :content => 'Lorem Ipsum')
         @panel.tabs.should have(1).tab
-      end      
+      end
       it "should create a panel with one tab with the right content" do
         @panel.add_tab('foo', :content => 'Lorem Ipsum')
-        @panel.tabs[0].content.should == 'Lorem Ipsum'       
+        @panel.tabs[0].content.should == 'Lorem Ipsum'
       end
       it "should create a tab with a specific id" do
         @panel.add_tab('foo', :content => 'Lorem Ipsum', :base_id => 'bar')
-        @panel.tabs[0].base_id.should == 'bar'               
+        @panel.tabs[0].base_id.should == 'bar'
       end
       it "should raise an exception if a tab with duplicate ID is added" do
-        lambda do 
+        lambda do
           @panel.add_tab('foo', :content => 'Lorem Ipsum', :base_id => 'bar')
           @panel.add_tab('foo 2', :content => 'Lorem Ipsum 2', :base_id => 'bar')
-        end.should raise_error 
+        end.should raise_error
       end
-    end  
-    
+    end
+
     describe "with block content" do
       it "should create a panel with one tab" do
         @panel.add_tab('foo') { "Lorem Ipsum" }
-        @panel.tabs.should have(1).tab        
+        @panel.tabs.should have(1).tab
       end
       it "should create a panel with one tab with the right content" do
         @panel.add_tab('foo') { "Lorem Ipsum" }
-        @panel.tabs[0].content.should == 'Lorem Ipsum'       
+        @panel.tabs[0].content.should == 'Lorem Ipsum'
       end
     end
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,6 @@
 Dir[File::expand_path(__FILE__ + "/../support/**/*.rb")].each{|file| require file}
 Ungemmer::ungem_gemspec
-
-begin
-  require File.dirname(__FILE__) + '/../../../../spec/spec_helper'
-rescue LoadError
-  puts "You need to install rspec in your base app"
-  exit
-end
-
 # require 'spec/rails'
-
-plugin_spec_dir = File.dirname(__FILE__)
-ActiveRecord::Base.logger = Logger.new(plugin_spec_dir + "/debug.log")
 
 # module ActiveSupport
 #   class TestCase
