5bab48e03e316176a8250255682b6e12c2452f80
Evan Dorn <evan@lrdesign.com>
2011-02-23 16:18:31 -0800

    specs for ERB views

--- lib/logical_tabs.rb
+++ lib/logical_tabs.rb
@@ -1,7 +1,6 @@
 
 module LogicalTabs
   class Railtie < Rails::Railtie
-    puts "------------------- Loading Railtie"
     require 'logical_tabs/helper'
 
     ActionController::Base.helper(LogicalTabs::Helper)
--- spec/rails3/app_root/config/environment.rb
+++ spec/rails3/app_root/config/environment.rb
@@ -1,6 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-p "------------------- Test app environment being required"
 
 # Initialize the rails application
 AppRoot::Application.initialize!
