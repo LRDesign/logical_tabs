0463e82e3d674dff6607cb0aea747132bc8435e3
Evan Dorn <evan@lrdesign.com>
2011-02-25 12:33:41 -0800

    generator switches between CSS and sass

--- lib/generators/logical_tabs/install/install_generator.rb
+++ lib/generators/logical_tabs/install/install_generator.rb
@@ -2,6 +2,17 @@ module LogicalTabs
   module Generators
     class InstallGenerator < Rails::Generators::Base
 
+      class_option  :css,
+                    :type => :string,
+                    :desc => "Type of CSS to install, (css, sass, or none), default css",
+                    :default => "css"
+
+      class_option  :js,
+                    :type => :string,
+                    :desc => "Type of JS to install, (prototype, jquery, or none), default prototype",
+                    :default => 'prototype'
+
+
       desc <<DESC
 Description:
 Copy LogicalTabs files to your application.
@@ -11,9 +22,23 @@ DESC
         @source_root ||= File.expand_path(File.join(File.dirname(__FILE__), 'templates'))
       end
 
-      def copy_files
-        copy_file 'javascripts/logical_tabs_prototype.js', 'public/javascripts/logical_tabs_prototype.js'
-        copy_file 'stylesheets/logical_tabs.css', 'public/stylesheets/logical_tabs.css'
+      def check_options
+        quit_with_options_error unless %w(css sass none).include?(options[:css])
+        quit_with_options_error unless %w(prototype jquery none).include?(options[:js])
+      end
+
+      def install_javascript
+        case options[:js]
+        when 'prototype':  copy_file 'javascripts/logical_tabs_prototype.js', 'public/javascripts/logical_tabs_prototype.js'
+        when 'jquery':  copy_file 'javascripts/logical_tabs_prototype.js', 'public/javascripts/logical_tabs_prototype.js'
+        end
+      end
+
+      def install_stylesheet
+        case options[:css]
+        when 'css': copy_file 'stylesheets/logical_tabs.css', 'public/stylesheets/logical_tabs.css'
+        when 'sass': copy_file 'stylesheets/sass/logical_tabs.sass', 'public/stylesheets/sass/logical_tabs.sass'
+        end
       end
 
       def add_javascript
@@ -21,7 +46,7 @@ DESC
       end
 
       def reminder
-        say (<<NOTICE
+        say <<NOTICE
 
 LogicalTabs installed!  Remember to add the stylesheet and javascript link tags.
 
@@ -30,8 +55,21 @@ LogicalTabs installed!  Remember to add the stylesheet and javascript link tags.
      -- OR --
   <%= javascript_include_tag 'logical_tabs_jquery.js' %>
 NOTICE
-        )
       end
+
+
+      private
+      def quit_with_options_error
+        say <<END_OF_ERROR
+
+Invalid option specified.  If options are specified:
+  --css must be one of [ css, sass, none ]
+  --js must be one of [ prototype, jquery, none ]
+
+END_OF_ERROR
+        exit(1)
+      end
+
     end
   end
 end
