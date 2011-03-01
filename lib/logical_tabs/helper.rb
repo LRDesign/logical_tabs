6ddb335cbaeb27c6329c55b990d08607702268b0
Evan Dorn <evan@lrdesign.com>
2011-03-01 00:29:29 -0800

    switched the name of the helper method

--- lib/logical_tabs/helper.rb
+++ lib/logical_tabs/helper.rb
@@ -2,7 +2,7 @@ module LogicalTabs
   require 'logical_tabs/tabbed_panel'
 
   module Helper
-    def create_tabbed_panel( options = {}, &block)
+    def tabbed_panel( options = {}, &block)
       @panel_count = defined?(@panel_count) ? @panel_count + 1 : 0
       options.merge!({:seq => @panel_count}) unless options[:base_id]
       tabbed_panel = TabbedPanel.new(self, options)
@@ -14,6 +14,8 @@ module LogicalTabs
       end
       return output
     end
+
+    alias_method :create_tabbed_panel, :tabbed_panel
 
     def wrap_in_div(id, &block)
       content_tag(:div, capture(&block), :class => 'wrapper', :id => id)
