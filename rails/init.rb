adc5991942d2b91ef45f5609eb24387f9af9f98c
Evan Dorn <evan@lrdesign.com>
2011-07-14 15:42:30 -0700

    made some fixes for rails 2

--- rails/init.rb
+++ rails/init.rb
@@ -1,9 +1,9 @@
-# require File.join(File.dirname(__FILE__), "lib", "logical_tabs", "helper")  
-ActionView::Base.send :include, LogicalTabs::Helper  
+# require File.join(File.dirname(__FILE__), "lib", "logical_tabs", "helper")
+ActionView::Base.send :include, LogicalTabs::Helper
 
-# FIX for engines model reloading issue in development mode                 
+# FIX for engines model reloading issue in development mode
 if ENV['RAILS_ENV'] != 'production'
 	load_paths.each do |path|
-		ActiveSupport::Dependencies.load_once_paths.delete(path)
+		ActiveSupport::Dependencies.autoload_once_paths.delete(path)
 	end
 end
